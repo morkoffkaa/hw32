92d096eec7d5e3f60fa95bbefd40c712adcbe7f4
Olena Kaliuzhna <kaliuzhna.el@gmail.com>
2023-03-30 11:31:36 +0300

    add hiding style for buttons

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
 const sliderLine = document.querySelector(".slider-line");
 
-offset = 0;
+let offset = 0;
 
 const buttonNext = document.querySelector(".slider-next");
-
-console.log(buttonNext);
 buttonNext.addEventListener("click", function() {
-    offset = -offset + 256;
-    if (offset > 768) {
-      offset=0;  
-    }
-    sliderLine.style.left = offset + "px";
+  offset = offset - 256;
+  if (offset < -768) {
+    offset = 0; 
+    buttonNext.style.display = "none";
+    buttonBack.style.display = "block";
+  }
+  sliderLine.style.left = offset + "px";
 });
 
-
+const buttonBack = document.querySelector(".slider-back");
+buttonBack.addEventListener("click", function() {
+  offset = offset + 256;
+  if (offset > 0) {
+    offset = -768;
+    buttonBack.style.display = "none"; 
+    buttonNext.style.display = "block";
+  }
+  sliderLine.style.left = offset + "px";
+});
